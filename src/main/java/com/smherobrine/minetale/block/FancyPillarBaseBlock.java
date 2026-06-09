397965e9f797b09328277a7a36b1d4b151242d8b
SMHerobrine <smpihlas@gmail.com>
2026-06-09 15:25:28 +0300

    Pillar Fin Fix

--- src/main/java/com/smherobrine/minetale/block/FancyPillarBaseBlock.java
+++ src/main/java/com/smherobrine/minetale/block/FancyPillarBaseBlock.java
@@ -2,29 +2,28 @@ package com.smherobrine.minetale.block;
 
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 
-public final class FancyPillarBaseBlock extends RotatedPillarBlock {
-	public static final EnumProperty<Direction> FACING = BlockStateProperties.HORIZONTAL_FACING;
+public final class FancyPillarBaseBlock extends Block {
+	public static final EnumProperty<Direction> FACING = BlockStateProperties.FACING;
 
 	public FancyPillarBaseBlock(BlockBehaviour.Properties properties) {
 		super(properties);
-		registerDefaultState(defaultBlockState().setValue(FACING, Direction.NORTH));
+		registerDefaultState(defaultBlockState().setValue(FACING, Direction.DOWN));
 	}
 
 	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<net.minecraft.world.level.block.Block, BlockState> builder) {
-		super.createBlockStateDefinition(builder);
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		builder.add(FACING);
 	}
 
 	@Override
 	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		return super.getStateForPlacement(context).setValue(FACING, context.getHorizontalDirection());
+		return defaultBlockState().setValue(FACING, context.getClickedFace().getOpposite());
 	}
 }
